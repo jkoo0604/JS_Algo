0188885750aea8a80ceeb30ec289e22e39036734
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2020-08-06 21:43:06 -0700

    0806-2

---
+++ multiplepointers.js
@@ -0,0 +1,41 @@
+// takes in two strings, checks whether the characters in first string form a subsequence of the characters in second. 
+// check whether the characters in the first string appear somewhere in the second, without their order changing
+
+function isSubsequence(str1, str2) {
+    
+    let i=0;
+    for (let char of str2) {
+        if (char === str1[i]) {
+            i++;
+            if (i >= str1.length) return true;
+        }
+    }
+    return false;
+  }
+
+console.log(isSubsequence('hello','hello world')) //t
+console.log(isSubsequence('sing','string')) //t
+console.log(isSubsequence('abc','abracadabra')) //t
+console.log(isSubsequence('abc','acb')) //f
+
+// given a sorted array of int and target avg, check if there is a pair of values in the array where the average of the pair equals the target average
+function averagePair(arr, avg){
+    // add whatever parameters you deem necessary - good luck!
+    var i = 0;
+    var j = arr.length-1;
+    while (i<j) {
+        if ((arr[i] + arr[j])/2 > avg) {
+            j--;
+        } else if ((arr[i] + arr[j])/2 < avg) {
+            i++;
+        } else {
+            return true;
+        }
+    }
+    return false;
+  }
+
+console.log(averagePair([1,2,3],2.5)) //t
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) //t
+console.log(averagePair([-1,0,3,4,5,6],4.1)) //f
+console.log(averagePair([],4)) //f
--- slidingwindow.js
+++ slidingwindow.js
@@ -1,5 +1,6 @@
+// given a string, return the length of the longest substring with all distinct characters
+
 function findLongestSubstring(str){
-    // add whatever parameters you deem necessary - good luck!
     if (str.length === 0) return 0;
     var start = 0;
     var end = 0;
@@ -16,16 +17,73 @@ function findLongestSubstring(str){
             maxStr = str.substring(start, end+1);
         }
         maxLen = Math.max(maxLen, end-start+1);
-        console.log(str.substring(start, end+1));
+        // console.log(str.substring(start, end+1));
         end++;
     }
-    return maxStr;
+    return maxLen;
   }
 
-// console.log(findLongestSubstring(''))
-// console.log(findLongestSubstring('rithmschool'))
-// console.log(findLongestSubstring('thisisawesome'))
-// console.log(findLongestSubstring('thecatinthehat'))
-// console.log(findLongestSubstring('bbbbbb'))
-console.log(findLongestSubstring('longestsubstring'))
-// console.log(findLongestSubstring('thisishowwedoit'))
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('rithmschool')) // 7
+console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thecatinthehat')) // 7
+console.log(findLongestSubstring('bbbbbb')) // 1
+console.log(findLongestSubstring('longestsubstring')) // 8
+console.log(findLongestSubstring('thisishowwedoit')) // 6
+
+
+console.log('*'.repeat(40));
+// given array of positive int and positive int, return min length of contiguous subarray of which the sum is greater than or equal to the testInt. if there isn't one, return 0
+function minSubArrayLen(arr, testInt) {
+    var start = 0;
+    var end = 0;
+    var sum = arr[start];
+    var minLen = Infinity;
+    while (start <= arr.length-1) {
+        if (sum < testInt && end+1 <= arr.length-1) {
+            end++;
+            sum += arr[end];
+        } else if (sum < testInt && end+1 >= arr.length) {
+            break;
+        } else {
+            minLen = Math.min(minLen, end-start+1);
+            sum -= arr[start];
+            start++;
+        }
+    }
+    
+    return minLen === Infinity ? 0 : minLen ; 
+}
+
+console.log(minSubArrayLen([2,3,1,2,4,3],7)) //2 ([4,3])
+console.log(minSubArrayLen([2,1,6,5,4],9)) //2 ([5,4])
+console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19],52)) //1 ([62])
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
+console.log(minSubArrayLen([4,3,3,8,1,2,3],11)) // 2
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
+
+console.log('*'.repeat(40));
+// given array of int and a num, find the maximum sum of subarray with the length of the number. subarray must consist of consecutive elements frmo the original array (e.g. [100, 300] is not a subarray in the first example)
+function maxSubarraySum(arr, num){
+    if (arr.length < num) return null;
+    var maxSum = 0;
+    var tempSum = 0;
+    for (let i=0; i<num; i++) {
+        tempSum += arr[i];
+    }
+    maxSum = tempSum;
+    for (let i=num; i<arr.length; i++) {
+        tempSum = tempSum - arr[i-num] + arr[i];
+        maxSum = Math.max(tempSum, maxSum);
+    }
+    return maxSum;
+    
+    
+  }
+
+console.log(maxSubarraySum([100,200,300,400],2)) //700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20],4)) //39
+console.log(maxSubarraySum([-3,4,0,-2,6,-1],2)) //5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) //5
+console.log(maxSubarraySum([2,3],3)) //null
