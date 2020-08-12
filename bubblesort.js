80fb4c9235b18f96a4b7b32ad72a19e3731b7780
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2020-08-11 23:32:09 -0700

    0811-3

--- bubblesort.js
+++ bubblesort.js
@@ -3,14 +3,17 @@ function bubbleSort(arr) {
     var i = 0;
     while (i < sorted) {
         var j = 1;
+        var swapped = false;
         while (j < sorted) {
             if (arr[j] < arr[j-1]) {
                 var temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
+                swapped = true;
             }
             j++;
         }
+        if (!swapped) break;
         sorted--;
         i++;
     }
@@ -19,3 +22,4 @@ function bubbleSort(arr) {
 
 console.log(bubbleSort([13,7,0,6,30]))
 console.log(bubbleSort([65,0,42,-5,8,32,1]))
+console.log(bubbleSort([1,2,3,4,5]))
