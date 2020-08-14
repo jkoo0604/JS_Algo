d4a07f4bb622432e0fc1f4b989e888e571afafa3
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2020-08-13 20:28:28 -0700

    0813-4

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -11,6 +11,22 @@ class SLL {
         this.tail = null;
         this.length = 0;
     }
+    
+    print() {
+        if (this.length === 0) console.log('empty');
+        else {
+            var runner = this.head;
+            console.log('******');
+            while (runner) {
+                console.log(runner.val);
+                runner = runner.next;
+            }
+            console.log('head: ', this.head);
+            console.log('tail: ', this.tail);
+            console.log('length: ', this.length);
+            console.log('******');
+        }
+    }
 
     push(val) {
         var newNode = new Node(val);
@@ -26,7 +42,7 @@ class SLL {
     }
 
     pop() {
-        if (!this.head) return undefined;
+        if (!this.head) return null;
         var runner = this.head;
         if (this.length === 1) {
             this.head = null;
@@ -45,7 +61,7 @@ class SLL {
     }
 
     shift() {
-        if (!this.head) return undefined;
+        if (!this.head) return null;
         var currentHead = this.head;
         if (this.length === 1) {
             this.head = null;
@@ -72,7 +88,7 @@ class SLL {
     }
 
     get(idx) {
-        if (!this.head || this.length-1 < idx) return undefined;
+        if (!this.head || this.length <= idx || idx < 0) return null;
         if (idx === 0) return this.head;    
         var runner = this.head;
         var counter = 0;
@@ -82,6 +98,57 @@ class SLL {
         }
         return runner;
     }
+
+    set(idx, val) { // update value at index
+        var node = this.get(idx);
+        if (!node) return false;
+        node.val = val;
+        return true;
+    }
+
+    insert(idx, val) {
+        if (idx < 0 || idx > this.length) return false;
+        if (idx === 0) this.unshift(val);
+        else if (idx === this.length) this.push(val);
+        else {
+            var prevNode = this.get(idx-1);
+            var newNode = new Node(val);
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+            this.length++;
+        }
+        return true;
+    }
+
+    remove(idx) {
+        if (idx < 0 || idx >= this.length) return null;
+        if (idx === 0) return this.shift();
+        if (idx === this.length-1) return this.pop();
+        var prevNode = this.get(idx-1);
+        var removed = prevNode.next;
+        prevNode.next = removed.next;
+        removed.next = null;
+        this.length--;
+        return removed;
+    }
+
+    reverse() {
+        if (this.length <= 1) return this;
+        var left = this.head;
+        var middle = left.next;
+        var right = middle.next;
+        this.head = this.tail;
+        this.tail = left;
+        left.next = null;
+        while (middle) {
+            middle.next = left;
+            left = middle;
+            middle = right;
+            if (right) right = right.next;
+        }
+        return this;
+
+    }
 }
 
 var list = new SLL();
@@ -90,14 +157,20 @@ var list = new SLL();
 // console.log(list);
 console.log(list.push(15));
 console.log(list.push(18));
-console.log(list.push(20));
-console.log(list.push(25));
-console.log(list.shift());
-console.log(list);
-console.log(list.unshift(3));
-console.log(list.get(3))
-console.log(list.get(2))
-console.log(list.get(1))
-console.log(list.get(5))
+// console.log(list.push(20));
+// console.log(list.push(25));
+// console.log(list.shift());
+// list.print();
+// console.log(list.unshift(3));
+list.print();
+// console.log(list.get(3))
+// console.log(list.get(5))
+// console.log(list.set(3,4))
+// console.log(list.insert(3,35));
+// list.print();
+// console.log(list.remove(3));
+// list.print();
+console.log(list.reverse());
+list.print();
 
 
