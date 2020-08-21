24aaa17075bc79a2d9c6708bb230d755391e8403
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2020-08-20 23:31:21 -0700

    0820

---
+++ priorityqueues.js
@@ -0,0 +1,38 @@
+class PriorityNode {
+    constructor(data, priority){
+        this.data = data;
+        this.priority = priority;
+        this.next = null;
+    }
+}
+
+class priorityQueue {
+    constructor(){
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+        this.highestPriority = new SLL();
+    }
+
+    // enqueue into the rear of the queue, if highest priority, update
+    // the highestPriority array / stack
+    enqueue(node){}
+
+    // dequeue the last node, no matter the priority.
+
+    // if this node was our previous highest priority, pop
+    // from the highestPriority array
+    dequeue(){}
+
+    // look at the front node
+    peekFront(){}
+
+    // look at the back node
+    peekBack(){}
+
+    // check if empty
+    isEmpty(){}
+
+    // return the final value in the .highestPriority
+    getHighestPriority(){}
+}
