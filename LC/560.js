ab4927af2739520e269eda2849a154b90a1a6b25
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2021-01-05 10:23:40 -0800

    0105

---
+++ LC/560.js
@@ -0,0 +1,34 @@
+/*
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let sum = 0;
+    let count = 0;
+    let map = new Map();
+    map.set(0,1);
+    
+    for (let i=0; i<nums.length; i++) {
+        sum += nums[i];
+        if (map.has(sum - k)) {
+            count += map.get(sum - k);
+        }
+        map.set(sum, (map.get(sum) || 0) + 1); 
+    }
+    return count;
+};
